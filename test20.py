22a94dc331be44c0b49bb50140501fd603aaa240
여철준 <jun091027naver.com>
2025-03-27 10:10:04 +0900

    큰수 코드

--- test20.py
+++ test20.py
@@ -1,24 +1,16 @@
 a,b = map(int,input().split())
 c = list(map(int,input().split()))
-c.sort()
+c.sort(reverse=True)
 d = 0
 i = 0
 while b > 0 and i < len(c):
     if c[i] <= b:
         b -= c[i]
         d += c[i]
-        e = c[i]
+        i += 1
     else:
         if i >= len(c):
             break
         else:
-            if b + e >= c[i]:
-                print(b,c[i],e,d)
-                b += e
-                b -= c[i]
-                d -= e
-                d += c[i]
-                print(b,c[i],e,d)
-    i += 1
+            i += 1
 print(d)
-print(c)
