c087feddc10ca820bb3b2937e20e2ba1aa50b82d
여철준 <jun091027naver.com>
2025-03-27 14:32:30 +0900

    반례 1~3개

--- test20.py
+++ test20.py
@@ -1,16 +1,31 @@
 a,b = map(int,input().split())
 c = list(map(int,input().split()))
 c.sort(reverse=True)
-d = 0
-i = 0
-while b > 0 and i < len(c):
-    if c[i] <= b:
-        b -= c[i]
-        d += c[i]
-        i += 1
-    else:
-        if i >= len(c):
-            break
-        else:
-            i += 1
-print(d)
+f = []
+b1 = b
+for k in range(0,len(c)-1):
+    if c[k] < b1:
+        b1 -= c[k]
+        d = c[k]
+        e = 1
+        j = k + 1
+        for i in range(k+1,len(c)):
+            while j < len(c):
+                if c[j] <= b1:
+                    b1 -= c[j]
+                    d += c[j]
+                    e += 1
+                j += 1
+                if e == 3:
+                    f.append(d)
+                    e = 1
+                    b1 = b - c[k]
+                    d = c[k]
+            j += i + 1
+            b1 = b - c[k]
+            e = 1
+            d = c[k]
+    b1 = b
+    e = 0
+    d = 0
+print(max(f))
